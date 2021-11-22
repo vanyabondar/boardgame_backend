a05cd4a7231e0771403e75907b7b667b0f595999
elohssa <ibshbohdap99@gmail.com>
2021-11-22 20:41:59 +0200

    Changed DB on Postgresql

--- bg_seller/bg_seller/settings.py
+++ bg_seller/bg_seller/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'bg_seller.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'boardgames_seller',
+        'USER': 'postgres',
+        'PASSWORD': '112358',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
