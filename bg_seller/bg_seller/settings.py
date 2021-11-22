4e46cdb8de078d028ee30b29be1ee0720c46a12b
elohssa <ibshbohdap99@gmail.com>
2021-11-22 22:58:50 +0200

    Added models Game, Author, Gamecategory

--- bg_seller/bg_seller/settings.py
+++ bg_seller/bg_seller/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'games.apps.GamesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- bg_seller/games/models.py
+++ bg_seller/games/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+	autor_name = models.CharField(max_length=100)
+	author_description = models.TextField()
+
+
+class GameCategory(models.Model):
+	game_category_name = models.CharField(max_length=100)
+	game_category_description = models.TextField()
+
+
+class Game(models.Model):
+	game_name = models.CharField(max_length=100)
+	cost = models.DecimalField(max_digits=8, decimal_places=2)
+	quantity = models.IntegerField()
+	game_description = models.TextField()
+	released_year = models.IntegerField()
+	playing_time = models.IntegerField()
+	min_age = models.IntegerField()
+	authors = models.ManyToManyField(
+		'games.Author',
+		blank=True,
+		)
+	game_category = models.ManyToManyField(
+		'games.GameCategory',
+		blank=True,
+		)
