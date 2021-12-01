4a8982fdf81818b5df32182e3bfe542dade8c701
elohssa <ibshbohdap99@gmail.com>
2021-12-02 01:37:08 +0200

    Filtering, searching and ordering was added

--- bg_seller/bg_seller/settings.py
+++ bg_seller/bg_seller/settings.py
@@ -141,7 +141,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 2,
+    'PAGE_SIZE': 100,
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.AllowAny',
     ),
--- bg_seller/games/views.py
+++ bg_seller/games/views.py
@@ -1,5 +1,6 @@
 # from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters
+from rest_framework import filters as filters_rest
+from django_filters import rest_framework as filters_dj
 from django.shortcuts import render
 from rest_framework import viewsets
 from games.models import Game, Author, GameCategory, Order
@@ -7,12 +8,15 @@ from games.serializers import GameSerializer, AuthorSerializer, GameCategorySeri
 from rest_framework import permissions
 from bg_seller.permissions import IsAdminOrReadOnly
 
+
 class GameViewSet(viewsets.ModelViewSet):
 	permission_classes = [IsAdminOrReadOnly]
 	queryset = Game.objects.all()
 	serializer_class = GameSerializer
-	filter_backends = [filters.SearchFilter,]
+	filter_backends = [filters_rest.SearchFilter, filters_dj.DjangoFilterBackend, filters_rest.OrderingFilter]
+	filter_fields = ['game_category', 'authors',]
 	search_fields = ['game_name',]
+	ordering_fields = ["cost", "playing_time", "relised_year"]
 	
 
 class AuthorViewSet(viewsets.ModelViewSet):
