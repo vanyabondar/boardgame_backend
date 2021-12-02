707e07eacf63dd9fd0f7b7fbf55bcec259bc43b3
vanyabondar <ibahbohdap99@gmail.com>
2021-12-02 16:50:24 +0200

    .env support was added.

--- bg_seller/settings.py
+++ bg_seller/settings.py
@@ -9,25 +9,25 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+from dotenv import dotenv_values
 from pathlib import Path
 import datetime
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+config = dotenv_values()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q@-xw+#7ub%dj-j0sz8_1(3piv_h2k#a()#j8dqg%sxlg2tznl'
+SECRET_KEY = config["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -83,11 +83,11 @@ WSGI_APPLICATION = 'bg_seller.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'boardgames_seller',
-        'USER': 'postgres',
-        'PASSWORD': '112358',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': config["DB_NAME"],
+        'USER': config["DB_USER"],
+        'PASSWORD': config["DB_PASSWORD"],
+        'HOST': config["DB_HOST"],
+        'PORT': config["DB_PORT"],
     }
 }
 
